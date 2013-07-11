779a3db3667817afa0f9e64f8f1df5fda0a7db9e
lcamtuf@gmail.com <lcamtuf@gmail.com@6a479be5-498b-bbd4-9786-411faf50c169>
2013-07-11 04:03:24 +0000

    Initial commit
    git-svn-id: http://american-fuzzy-lop.googlecode.com/svn/trunk@7 6a479be5-498b-bbd4-9786-411faf50c169

---
+++ afl-as.h
@@ -0,0 +1,167 @@
+/*
+   american fuzzy lop - injectable parts
+   -------------------------------------
+
+   Written and maintained by Michal Zalewski <lcamtuf@google.com>
+
+   Copyright 2013 Google Inc. All rights reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at:
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   This file houses the assembly-level instrumentation injected into fuzzed
+   programs. The instrumentation stores pairs of data: identifiers of the
+   currently executing line and the line that executed immediately before.
+
+   The assembly code shown below is designed for debug-enabled (-g), 32-bit
+   x86 output produced by GCC for C/C++ programs. Porting to 64-bit is trivial.
+
+   In principle, similar code should be easy to inject into any well-behaved
+   binary-only code (e.g., using DynamoRIO). Calls and jumps offer natural
+   targets for instrumentation, and should offer comparable probe density.
+
+ */
+
+#ifndef _HAVE_AFL_AS_H
+#define _HAVE_AFL_AS_H
+
+#include "config.h"
+#include "types.h"
+
+#define STRINGIFY_INTERNAL(x) #x
+#define STRINGIFY(x) STRINGIFY_INTERNAL(x)
+
+static const u8* trampoline_fmt =
+
+  "\n"
+  "/* --- AFL TRAMPOLINE --- */\n"
+  "\n"
+  ".align 8\n"
+  "pushl %%ecx\n"
+  "pushl %%eax\n"
+  "movl $0x%08x, %%ecx\n"
+  "call __afl_maybe_log\n"
+  "popl %%eax\n"
+  "popl %%ecx\n\n";
+
+static const u8* main_payload = 
+
+  "\n"
+  "/* --- AFL MAIN PAYLOAD --- */\n"
+  "\n"
+  ".text\n"
+  ".align 8\n"
+  "\n"
+  "__afl_maybe_log:\n"
+  "\n"
+  "  lahf\n"
+  "  movb %ah, __afl_saved_flags\n"
+  "\n"
+  "  /* Check if SHM region is already mapped. */\n"
+  "\n"
+  "  movl __afl_area_ptr, %eax\n"
+  "  testl %eax, %eax\n"
+  "  je __afl_setup\n"
+  "\n"
+  "__afl_store:\n"
+  "\n"
+  "  /* Calculate and store hit for the code location specified in ecx. */\n"
+  "\n"
+  "  pushw %cx\n"
+  "\n"
+#ifdef COVERAGE_ONLY
+  "  shrl $8, %ecx\n"
+#else
+  "  movb __afl_prev_loc, %cl\n"
+  "  movb %ch, __afl_prev_loc\n"
+  "  shlb $2, %cl\n"
+  "  shrw $2, %cx\n"
+#endif /* ^COVERAGE_ONLY */
+  "  addl %ecx, %eax\n"
+  "\n"
+  "  popw %cx\n"
+  "  orb %cl, (%eax)\n"
+  "\n"
+  "  movb __afl_saved_flags, %ah\n"
+  "  sahf\n"
+  "  ret\n"
+  "\n"
+  ".align 8\n"
+  "\n"
+  "__afl_setup:\n"
+  "\n"
+  "  /* Do not retry setup if we had previous failures. */\n"
+  "\n"
+  "  cmpb $0, __afl_setup_failure\n"
+  "  je __afl_setup2\n"
+  "\n"
+  "__afl_return:\n"
+  "\n"
+  "  movb __afl_saved_flags, %ah\n"
+  "  sahf\n"
+  "  ret\n"
+  "\n"
+  "__afl_setup2:\n"
+  "\n"
+  "  /* Map SHM, jumping to __afl_setup_abort if something goes wrong. */\n"
+  "\n"
+  "  pushl %ecx\n"
+  "  pushl %edx\n"
+  "\n"
+  "  pushl $.AFL_SHM_ID\n"
+  "  call  getenv\n"
+  "  addl  $4, %esp\n"
+  "\n"
+  "  testl %eax, %eax\n"
+  "  je __afl_setup_abort\n"
+  "\n"
+  "  pushl %eax\n"
+  "  call  atoi\n"
+  "  addl  $4, %esp\n"
+  "\n"
+  "  pushl $0          /* shmat flags    */\n"
+  "  pushl $0          /* requested addr */\n"
+  "  pushl %eax        /* SHM ID         */\n"
+  "\n"
+  "  call shmat\n"
+  "  addl $12, %esp\n"
+  "\n"
+  "  testl %eax, %eax\n"
+  "  je __afl_setup_abort\n"
+  "\n"
+  "  /* Store the address of the SHM region. */\n"
+  "\n"
+  "  movl %eax, __afl_area_ptr\n"
+  "\n"
+  "  popl %edx\n"
+  "  popl %ecx\n"
+  "\n"
+  "  jmp __afl_store\n"
+  "\n"
+  "__afl_setup_abort:\n"
+  "\n"
+  "  /* Record setup failure so that we don't keep calling\n"
+  "     shmget() / shmat() over and over again. */\n"
+  "\n"
+  "  incb __afl_setup_failure\n"
+  "  popl %edx\n"
+  "  popl %ecx\n"
+  "  jmp __afl_return\n"
+  "\n"
+  ".AFL_VARS:\n"
+  "\n"
+  "  .comm   __afl_area_ptr, 4, 32\n"
+  "  .comm   __afl_setup_failure, 1, 32\n"
+#ifndef COVERAGE_ONLY
+  "  .comm   __afl_prev_loc, 1, 32\n"
+#endif /* !COVERAGE_ONLY */
+  "  .comm   __afl_saved_flags, 1, 32\n"
+  "\n"
+  ".AFL_SHM_ID:\n"
+  "  .string \"" SHM_ENV_VAR "\"\n"
+  "\n";
+
+#endif /* !_HAVE_AFL_AS_H */
---
+++ config.h
@@ -0,0 +1,118 @@
+/*
+   american fuzzy lop - vaguely configurable bits
+   ----------------------------------------------
+
+   Written and maintained by Michal Zalewski <lcamtuf@google.com>
+
+   Copyright 2013 Google Inc. All rights reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at:
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ */
+
+#ifndef _HAVE_CONFIG_H
+#define _HAVE_CONFIG_H
+
+#include "types.h"
+
+/* Maximum allocator request size (keep well under INT_MAX): */
+
+#define MAX_ALLOC           0x40000000
+
+/* Maximum size of input file (keep under 100MB): */
+
+#define MAX_FILE            (1 * 1000 * 1000)
+
+/* Comment out to disable terminal colors: */
+
+#define USE_COLOR
+
+/* Maximum line length passed from GCC to 'as': */
+
+#define MAX_AS_LINE         8192
+
+/* Default timeout for fuzzed code (milliseconds): */
+
+#define EXEC_TIMEOUT        3000
+
+/* Default memory limit for child process (MB): */
+
+#define MEM_LIMIT           100
+
+/* Number of calibration cycles per every initial test case: */
+
+#define CAL_CYCLES          4
+
+/* Distinctive exit code used to indicate failed execution: */
+
+#define EXEC_FAIL           0x55
+
+/* Maximum number of subsequent hangs before abandoning an input file: */
+
+#define HANG_LIMIT          20
+
+/* Number of random tweaks during a single 'havoc' stage: */
+
+#define HAVOC_CYCLES        5000
+
+/* Max havoc stacking (the average is half of this amount): */
+
+#define HAVOC_STACKING      10
+
+/* Caps on block size for cloning and deletion operation: */
+
+#define HAVOC_MAX_BLOCK     100
+#define HAVOC_MAX_PERCENT   75
+
+/* Interval between reseeding PRNG (values returned): */
+
+#define RESEED_RNG          10000
+
+/* Environment variable used to pass SHM ID to the called program. */
+
+#define SHM_ENV_VAR         "__AFL_SHM_ID"
+
+/* Uncomment this to use inferior coverage-based instrumentation. */
+
+// #define COVERAGE_ONLY
+
+/* List of interesting values to use in fuzzing. */
+
+#define INTERESTING_8 \
+  -128,          /* Overflow signed 8-bit when decremented  */ \
+  -1,            /*                                         */ \
+   0,            /*                                         */ \
+   1,            /*                                         */ \
+   16,           /* One-off with common buffer size         */ \
+   32,           /* One-off with common buffer size         */ \
+   64,           /* One-off with common buffer size         */ \
+   100,          /* One-off with common buffer size         */ \
+   127           /* Overflow signed 8-bit when incremented  */
+
+#define INTERESTING_16 \
+  -32768,        /* Overflow signed 16-bit when decremented */ \
+  -129,          /* Overflow signed 8-bit                   */ \
+   128,          /* Overflow signed 8-bit                   */ \
+   255,          /* Overflow unsig 8-bit when incremented   */ \
+   256,          /* Overflow unsig 8-bit                    */ \
+   512,          /* One-off with common buffer size         */ \
+   1000,         /* One-off with common buffer size         */ \
+   1024,         /* One-off with common buffer size         */ \
+   4096,         /* One-off with common buffer size         */ \
+   32767         /* Overflow signed 16-bit when incremented */
+
+#define INTERESTING_32 \
+  -2147483648LL, /* Overflow signed 32-bit when decremented */ \
+  -100000000,    /* Large negative number (100M)            */ \
+  -32769,        /* Overflow signed 16-bit                  */ \
+   32768,        /* Overflow signed 16-bit                  */ \
+   65535,        /* Overflow unsig 16-bit when incremented  */ \
+   65536,        /* Overflow unsig 16 bit                   */ \
+   100000000,    /* Large positive number (100M)            */ \
+   2147483647    /* Overflow signed 32-bit when incremented */
+
+#endif /* ! _HAVE_CONFIG_H */
